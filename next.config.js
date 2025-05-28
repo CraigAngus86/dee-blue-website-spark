df419b2859839647725b2b222b29c5d33026dba5
CraigAngus86 <c.r.angus@btinternet.com>
2025-05-28 15:35:56 +0000

    Add sizes property for Vercel project creation

--- next.config.js
+++ next.config.js
@@ -2,36 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      {
-        protocol: 'https', 
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'bbbxhwaixjjxgboeiktq.supabase.co',
-      }
-    ],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    sizes: '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-  },
-  env: {
-    // Make sure we export any public env vars if needed
-  },
-  experimental: {
-    outputFileTracingExcludes: {
-      '*': ['.env*']
-    }
-  },
 };
 module.exports = nextConfig;
